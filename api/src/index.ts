258cd7ec2bba876b00ad5e0f89edcbb60364d528
Yuval Shavit <yuvals@wix.com>
2024-03-04 12:08:43 +0200

    add files

---
+++ api/src/index.ts
@@ -0,0 +1,22 @@
+/**
+ * @api {post} /product Create a new Product
+ * @apiName CreateProduct
+ * @apiGroup Product
+ *
+ * @apiBody {String} id ID of the product
+ * @apiBody {String} name=Ball Name of the Product
+ * @apiBody {number} quantity Quantity currently left in inventory
+ * @apiBody {number} price Product price
+ *
+ * @apiSuccess {Number} id         The new Product ID.
+ */
+
+/**
+ * @api {get} /product/:id Get Product
+ * @apiName GetProduct
+ * @apiGroup Product
+ *
+ * @apiParam {String} id Product unique ID.
+ *
+ * @apiSuccess {Object} product The new Product.
+ */
